446c78bd3d55edb13f26e3af73f7c404c67aadf2
Ignacio <54366484+auyeros@users.noreply.github.com>
2020-04-01 18:19:01 -0300

    Fix Palette Route

--- colors-app/src/colorHelpers.js
+++ colors-app/src/colorHelpers.js
@@ -1,5 +1,4 @@
 import chroma from "chroma-js";
-import seedColors from "./seedColors";
 const levels = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900];
 
 function generatePalette(starterPalette) {
