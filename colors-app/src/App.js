b2436a43e70c990b2a02a39200bb4f1fddd69fae
Ignacio <54366484+auyeros@users.noreply.github.com>
2020-04-14 00:57:16 -0300

    Add NewPaletteForm and Link

--- colors-app/src/App.js
+++ colors-app/src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
+import { generatePalette } from "./colorHelpers";
 import Palette from "./Palette.js";
+import NewPaletteForm from "./NewPaletteForm";
 import PaletteList from "./PaletteList";
 import SingleColorPalette from "./SingleColorPalette";
 import seedColors from "./seedColors";
-import { generatePalette } from "./colorHelpers";
 
 class App extends Component {
     findPalette(id) {
-        return seedColors.find(function(palette) {
+        return seedColors.find(function (palette) {
             return palette.id === id;
         });
     }
@@ -17,15 +18,20 @@ class App extends Component {
             <Switch>
                 <Route
                     exact
+                    path="/palette/new"
+                    render={() => <NewPaletteForm></NewPaletteForm>}
+                />
+                <Route
+                    exact
                     path="/"
-                    render={routeProps => (
+                    render={(routeProps) => (
                         <PaletteList palettes={seedColors} {...routeProps} />
                     )}
                 />
                 <Route
                     exact
                     path="/palette/:id"
-                    render={routeProps => (
+                    render={(routeProps) => (
                         <Palette
                             palette={generatePalette(
                                 this.findPalette(routeProps.match.params.id)
@@ -36,7 +42,7 @@ class App extends Component {
                 <Route
                     exact
                     path="/palette/:paletteId/:colorId"
-                    render={routeProps => (
+                    render={(routeProps) => (
                         <SingleColorPalette
                             colorId={routeProps.match.params.colorId}
                             palette={generatePalette(
